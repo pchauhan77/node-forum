87b5010b985ef2393ad4f4ca5d37251e140a82df
tshepo mafahle <tshepo.mogano@villavest.co.za>
2022-02-23 17:10:36 +0200

    updated autoincrease middleware

--- middleware/autoincrease.js
+++ middleware/autoincrease.js
@@ -2,16 +2,17 @@ function fetchMock(url){
     let returnReponse = Math.random()
     returnReponse = ((returnReponse >= 0.5)? true : false)
     return (returnReponse === true ? 'Success' : 'Error')
+}
+
+export async function main(req, res, next){
+  // let reponse = await fetchMock('https://example.com')
+  let waitingTime
+  let n = 1
+  while(reponse === 'Error'){
+    n++
+    waitingTime = 100*(n**(2)) + 100
+    // reponse = await fetchMock('https://example.com')
+    setTimeout(() => {
+    }, waitingTime)
   }
-  async function main(req, res){
-   // let reponse = await fetchMock('https://example.com')
-    let waitingTime
-    let n = 1
-    while(reponse === 'Error'){
-      n++
-      waitingTime = 100*(n**(2)) + 100
-     // reponse = await fetchMock('https://example.com')
-      setTimeout(() => {
-      }, waitingTime)
-    }
-  };
+};
